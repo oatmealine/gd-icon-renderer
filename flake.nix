39e38c851eb05c8324716bb5da4f3d11ffa52b2d
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2024-01-21 13:29:34 +0300

    bump ver to 0.2.0

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         packages = flake-utils.lib.flattenTree rec {
           gd-icon-renderer = pkgs.crystal.buildCrystalPackage {
             pname = "gd-icon-renderer";
-            version = "0.1.0";
+            version = "0.2.0";
 
             src = ./.;
 
